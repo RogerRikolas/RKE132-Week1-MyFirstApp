1bbb93f0d46951ad6f3371ef9d4d48750bf0d6d1
RogerRikolas <roger.rikolas@tktk.ee>
2022-09-04 17:16:26 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,9 @@
+﻿
+// rakendus küsib kasutjal sisestada nime
+// tuleb tervitus
+
+Console.WriteLine("Mis on sinu nimi?");
+string userName = Console.ReadLine(); //input
+Console.WriteLine("Tere " + userName + "!");
+// string interpolation
+Console.WriteLine($"Hello, {userName} !"); //output
